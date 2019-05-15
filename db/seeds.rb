0e0e67e2d4577cf481495ec4f67233630aa79e22
Adele Soullier <adele.soullier@gmail.com>
2019-05-15 16:58:18 +0200

    created 2 mvp with no scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+10.times do
+  resto = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    telephone: Faker::Number.leading_zero_number(10),
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  3.times do
+    comments = ["good","I love their desserts", "could be cleaner", "awesome", "awful", "not that bad", "waiter was very polite", "bread was frozen", "ideal for groups and families"]
+    Review.create!(
+      content: comments.sample,
+      rating: rand(1..5),
+      restaurant_id: resto.id
+    )
+  end
+end
